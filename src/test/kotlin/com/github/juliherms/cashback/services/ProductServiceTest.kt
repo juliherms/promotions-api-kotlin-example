39289ef6c8e574e3834a3467d8a4c01ac13e1449
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2021-03-27 18:54:08 -0300

    create test case for Product Service

--- src/test/kotlin/com/github/juliherms/cashback/services/ProductServiceTest.kt
+++ src/test/kotlin/com/github/juliherms/cashback/services/ProductServiceTest.kt
@@ -7,33 +7,36 @@ import com.github.juliherms.cashback.service.ProductService
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.BDDMockito
-import org.mockito.Mock
+import org.mockito.Mockito.`when`
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
 /**
  * This class responsible to implement test case for Product service
  */
-@SpringBootTest()
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
 class ProductServiceTest {
 
     @Autowired
     val productService : ProductService? = null
 
-    @Mock
+    @MockBean
     private val productRepository: ProductRepository? = null
 
     private val DESCRIPTION = "Ração Premiere"
 
-    private fun product() : Product = Product("Ração Premiere","1")
+    private fun product() : Product = Product(DESCRIPTION,"1")
 
-    private fun productDto() : ProductDTO = ProductDTO(DESCRIPTION)
+    private fun productDto() : ProductDTO = ProductDTO(DESCRIPTION,"1")
 
     @BeforeEach
     fun setUp() {
 
-        BDDMockito.given(productRepository?.save(product())).willReturn(product())
 
     }
     /**
@@ -45,16 +48,6 @@ class ProductServiceTest {
         BDDMockito.given(productRepository?.findByDescription(DESCRIPTION)).willReturn(product())
 
         val product: Product? = productService?.findByDescription(productDto())
-        Assertions.assertNotNull(product)
-    }
-
-    /**
-     * Test save product
-     */
-    @Test
-    fun testPersistProductSuccess() {
-
-        val product: Product? = productService?.persist(productDto())
         Assertions.assertNotNull(product)
     }
 }
